738283728edfa63e4f39ae23044b37e6e71552e4
soohyeon <meltapplee@gmail.com>
2024-07-16 12:50:47 +0900

    merge :: 유저 회원가입 구현

--- src/main/java/org/example/demo/domian/user/controller/UserController.java
+++ src/main/java/org/example/demo/domian/user/controller/UserController.java
@@ -2,8 +2,10 @@ package org.example.demo.domian.user.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.example.demo.domian.user.dto.request.SigninRequest;
+import org.example.demo.domian.user.dto.request.SignupRequest;
 import org.example.demo.domian.user.dto.response.TokenResponse;
 import org.example.demo.domian.user.service.UserLoginService;
+import org.example.demo.domian.user.service.UserSignupService;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,6 +20,12 @@ import javax.validation.Valid;
 @RequestMapping("/user")
 public class UserController {
     private final UserLoginService userLoginService;
+    private final UserSignupService userSignupService;
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/signup")
+    public TokenResponse signup(@RequestBody @Valid SignupRequest request) { return userSignupService.execute(request); }
+
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/signin")
