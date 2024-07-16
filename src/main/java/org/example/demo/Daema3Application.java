53f0b62669715417d5613fa42743555f7bd3bb98
soohyeon <meltapplee@gmail.com>
2024-07-16 10:43:44 +0900

    setting

---
+++ src/main/java/org/example/demo/Daema3Application.java
@@ -0,0 +1,13 @@
+package org.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Daema3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Daema3Application.class, args);
+    }
+
+}
