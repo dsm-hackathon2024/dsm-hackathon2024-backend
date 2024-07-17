55aa390617ad3e7085eaca4f0dbcc70ddc9ec536
soohyeon <meltapplee@gmail.com>
2024-07-17 09:09:52 +0900

    refactor :: exception

--- src/main/java/org/example/demo/domian/user/service/UserMyPagesService.java
+++ src/main/java/org/example/demo/domian/user/service/UserMyPagesService.java
@@ -3,6 +3,7 @@ package org.example.demo.domian.user.service;
 import lombok.RequiredArgsConstructor;
 import org.example.demo.domian.user.dto.response.UserMyPagesResponse;
 import org.example.demo.domian.user.entity.User;
+import org.example.demo.domian.user.exception.UserNotFoundException;
 import org.example.demo.domian.user.repository.UserRepository;
 import org.example.demo.global.aws.S3Util;
 import org.springframework.stereotype.Service;
@@ -14,9 +15,8 @@ public class UserMyPagesService {
     private final S3Util s3Util;
 
     public UserMyPagesResponse userMyPagesService(String userId){
-        User user = userRepository.findById(userId).orElseThrow();
+        User user = userRepository.findById(userId).orElseThrow(() ->UserNotFoundException.EXCEPTION);
 
         return new UserMyPagesResponse(user.getScore(), user.getId(), user.getUsername(), s3Util.getProfileImageUrl(user.getProfile()));
-
     }
 }
